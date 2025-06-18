47b41f69bfcbf22fbe43f8332f8c6529ce876a41
mahmoudalieid <alim46792@gemail.com>
2025-06-18 13:07:43 +0300

    feat: update login functionality to validate user credentials

--- controllers/user.js
+++ controllers/user.js
@@ -1,29 +1,38 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 // Create a new user
-router.post('/login', async (req, res) => {
-  const { name, email } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const user = await prisma.user.create({
-      data: { name, email },
+    const user = await prisma.user.findUnique({
+      where: { email },
     });
-    res.status(201).json(user);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+    res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ error: 'Error creating user' });
+    res.status(500).json({ error: "Error logging in user" });
   }
 });
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await prisma.user.create({
-      data: { name, email, password },
+      data: { name, email, password: hashedPassword },
     });
     res.status(201).json(user);
   } catch (error) {
-    res.status(500).json({ error: 'Error registering user' });
+    res.status(500).json({ error: "Error registering user" });
   }
 });
